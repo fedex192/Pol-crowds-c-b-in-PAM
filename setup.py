64248d005c73fd656c697a0d74e6416c305c4d3b
fedex192 <42683983+fedex192@users.noreply.github.com>
2020-05-21 01:57:16 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools  # important
-from distutils.core import setup
-from Cython.Build import cythonize
-setup(ext_modules=cythonize("newexplorargsim2.pyx", build_dir="build"),
-
-#setup(ext_modules=cythonize("newcagent.pyx", build_dir="build"),
-                                           script_args=['build'], 
-                                           options={'build':{'build_lib':'.'}})
